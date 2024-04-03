d14bd6969591f44fd21c9a9f785292221ec69a9c
albertine <albertine.dubois@cea.fr>
2024-04-03 11:57:23 +0200

    second commit

--- extract_suv.py
+++ extract_suv.py
@@ -1,11 +1,24 @@
 import re
+import argparse
 
-fic = "./20200527152905.861802.ig.tum"
 
-with open(fic, "r") as f:
-	lines = f.read()
+#fic = "./20200527152905.861802.ig.tum"
 
-m = re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)",lines, re.M)
 
-d = {el[0]:float(el[1]) for el in m}
-print(d)
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fichier", help="indiquer le nom du fichier", type=str)
+    return parser.parse_args()
+
+def do_it(*arg):
+    with open(*arg, "r") as f:
+        lines = f.read()
+
+        m = re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)",lines, re.M)
+
+        d = {el[0]:float(el[1]) for el in m}
+        print(d)
+
+if __name__ == "__main__":
+    arg = parse()
+    do_it(arg.fichier)
